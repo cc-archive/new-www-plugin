c2dab45e262ee8ce14a188c07168edd45ea093ba
Floyd Mann <floyd@affinitybridge.com>
2016-05-27 10:33:28 -0700

    refs #180. Linking profile image to author profile.

--- cc-widgets/widgets/cc-news-features-widget.php
+++ cc-widgets/widgets/cc-news-features-widget.php
@@ -50,9 +50,9 @@ class CreativeCommons_News_Features_Widget extends WP_Widget {
                     <div class="title"><a href="<?php print $url; ?>"><?php print get_the_title() ?></a></div>
                     <div class="author-wrapper">
                       <div class="author-image">
-                      <?php
-                        $author_bio_avatar_size = apply_filters( 'twentysixteen_author_bio_avatar_size', 38 );
-                        echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
+                        <?php
+                          $author_bio_avatar_size = apply_filters( 'twentysixteen_author_bio_avatar_size', 38 );
+                          echo '<a href="' . get_author_posts_url(get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' )) . '">' . get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size ) . '</a>';
                         ?>
                       </div>
                       <div class="author-info-group">
